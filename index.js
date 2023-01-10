f3164cec5f0105c9b347b1d0dc90ef4e145a623e
Oskarnn <nygren.a.oskar@gmail.com>
2023-01-10 12:49:25 +0100

    First

---
+++ index.js
@@ -0,0 +1,27 @@
+// alert("Hello world");
+
+// let array = [1, 2, 4, 654, 234, 33, 654, 156, 55, 66];
+
+
+// array.forEach(function(newArray){
+//  if(newArray < 40) {
+//     console.log(newArray)
+//  }
+//  else{
+//     console.log("För stort") 
+//  }
+// });
+
+Vue.createApp({
+data(){
+    return {
+        title: 'HEJ ALLA'
+}
+}
+})
+
+// "mount" berätta vad i html dokumentet (vilken widget) vi ska jobba med
+//  exporterar funktioen till html
+.mount("#app")
+
+

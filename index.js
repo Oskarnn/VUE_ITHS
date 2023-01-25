885ba997ae0e16321def109939f224a0a10d7382
Oskarnn <nygren.a.oskar@gmail.com>
2023-01-25 14:51:23 +0100

    modul 8

--- index.js
+++ index.js
@@ -1159,3 +1159,181 @@
 // })
 
 // app.mount('#app')
+
+// UPPGIFT 5 (VG)
+
+// MODUL 7 Props
+
+// export default {
+//   props: {
+//     // Basic type check
+//     //  (`null` and `undefined` values will allow any type)
+//     propA: Number,
+//     // Multiple possible types
+//     propB: [String, Number],
+//     // Required string
+//     propC: {
+//       type: String,
+//       required: true
+//     },
+//     // Number with a default value
+//     propD: {
+//       type: Number,
+//       default: 100
+//     },
+//     // Object with a default value
+//     propE: {
+//       type: Object,
+//       // Object or array defaults must be returned from
+//       // a factory function. The function receives the raw
+//       // props received by the component as the argument.
+//       default(rawProps) {
+//         return { message: 'hello' }
+//       }
+//     },
+//     // Custom validator function
+//     propF: {
+//       validator(value) {
+//         // The value must match one of these strings
+//         return ['success', 'warning', 'danger'].includes(value)
+//       }
+//     },
+//     // Function with a default value
+//     propG: {
+//       type: Function,
+//       // Unlike object or array default, this is not a factory function - this is a function to serve as a default value
+//       default() {
+//         return 'Default function'
+//       }
+//     }
+//   }
+// }
+
+
+// UPPGIFT 1
+// HTML
+
+// <!DOCTYPE html>
+// <html lang="sv">
+//   <head>
+//     <meta charset="utf-8">
+//     <title>“name”</title>
+//     <meta content="initial-scale=1, width=device-width" name="viewport">
+//   </head>
+//   <body>
+//     <div id="app">
+//       <greet name="Alice"></greet> <!-- Välkommen, Alice! -->
+//       <greet name="Bob"></greet> <!-- Välkommen, Bob! -->
+//     </div>
+//     <script src="https://unpkg.com/vue@3"></script>
+//     <script src="index.js"></script>
+//   </body>
+// </html>
+
+// SCRIPT
+
+// const app = Vue.createApp({})
+
+// app.component('greet', {
+//   data(){
+//     return {
+//       namn: this.name
+//     }
+//   },
+//   props: ['name'],
+//   template:`
+//   <div>Välkommen, {{this.namn}}!</div>
+//   `
+// })
+
+// app.mount('#app')
+
+// UPPGIFT 2
+
+
+
+// const app = Vue.createApp({})
+
+// app.component('greet', {
+//   data(){
+//     return {
+//       namn: this.playerName
+//     }
+//   },
+
+//   props: { 
+//     playerName: String
+
+//   },
+//   template:`
+//   <div>Välkommen, {{this.playerName}}!</div>
+//   `
+
+// })
+
+// app.mount('#app')
+
+// // UPPGIFT 3
+
+
+// const app = Vue.createApp({})
+
+// app.component('greet', {
+//   data(){
+//     return {
+//       namn: this.playerName
+//     }
+//   },
+
+//   props: { 
+//     playerName: { type: String, required: true }
+
+//   },
+//   template:`
+//   <div>Välkommen, {{this.playerName}}!</div>
+//   `
+
+// })
+
+// app.mount('#app')
+
+// // UPPGIFT 4
+
+// const app = Vue.createApp({})
+
+// app.component('greet', {
+//   data(){
+//     return {
+//       namn: this.playerName
+//     }
+//   },
+
+//   props: { 
+//     playerName: { type: String, default: "Anonym" }
+
+//   },
+//   template:`
+//   <div>Välkommen, {{this.playerName}}!</div>
+//   `
+
+// })
+
+// app.mount('#app')
+
+// ELLER
+
+// const app = Vue.createApp({})
+
+// app.component('greet', {
+ 
+//   props: { 
+//     playerName: { type: String }
+
+//   },
+//   template:`
+//   <div>Välkommen, {{playerName  ? playerName : 'Anonym'}}!</div>
+//   `
+
+// })
+
+// app.mount('#app')
